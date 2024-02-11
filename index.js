5bcd81d3d9d144c6ae85002186475203b04d1b3d
dzjeri <2405godnadvore@gmail.com>
2024-02-11 07:13:15 +0300

    removed dead code

--- index.js
+++ index.js
@@ -17,24 +17,6 @@ app.use(express.json());
 app.use(cors());
 app.use(requestLogger);
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-];
-
 // "This notes is not saved on server" -> services/notes -> getAll() -> concat(nonexisting)
 
 app.get('/', (request, response) => {
